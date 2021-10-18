722736bb3e94d0414d619b63c074442fd40f4e4e
Crowdbotics <team@crowdbotics.com>
2021-10-18 21:57:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lingering_wave_31527",
+  url: "https://lingering_wave_31527.botics.co",
+  api: "https://lingering_wave_31527.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
